7ca8a1926eb786a53cdb64973beefce7f0788172
isaachumphries <isaacjhumphries@gmail.com>
2026-09-21 17:33:17 -0500

    prog1 completed, verify

--- prog1.c
+++ prog1.c
@@ -1,15 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
 
-    int n = 100;
-    printf("Enter a # of words to store: ");
+    int n;
+    printf("Enter a # of strings to store: ");
     scanf("%d", &n);
+    getchar();
 
-    printf("\nEnter you words below\n---------------------\n");
+    char **words = malloc(n * sizeof(char *));
+    char buff[513];
+    printf("\nEnter you strings below\n---------------------\n");
 
-    char words[n][512];
-    for (int i = 0; i <= n; i++) {
-        fgets(words[i], sizeof(words[i]), stdin);
+    for (int i = 0; i < n; i++) {
+        fgets(buff, sizeof(buff), stdin);
+        size_t len = strlen(buff);
+        if (len > 0 && buff[len - 1] == '\n') {
+            buff[--len] = '\0';
+        } else {
+            int c;
+            while ((c = getchar()) != '\n')
+                ;
+        }
+        words[i] = malloc(len + 1);
+        strcpy(words[i], buff);
     }
+
+    printf("words stored:\n{");
+    for (int i = 0; i < n - 1; i++) {
+        printf("\'%s\', ", words[i]);
+    }
+    printf("\'%s\'}", words[n - 1]);
+
+    return 0;
 }
