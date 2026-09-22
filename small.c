d295e533ee8a7fcb0be91501f2238759e3332839
isaachumphries <isaacjhumphries@gmail.com>
2026-09-21 19:12:47 -0500

    final, problem 3

--- small.c
+++ small.c
@@ -1,2 +1,1 @@
-#include <stdio.h>
-int main(){char a[]={' ','X',' '};for(int i=0;i<5;i++){for(int j=0;j<5;j++){putchar(a[i%2]);if((i%2==1&&j<4)||i%2==0){putchar(a[i%2+1]);}}putchar('\n');}return 0;}
+int main(int i){for(;i<54;i++)__builtin_putchar(i%21%11?88-i%21%2*56:10);}
